4247580863991115982034d435db575c34fd38c3
Didy Septiyono <didy@didy.project>
2018-07-20 06:39:36 +0700

    test

--- bot.js
+++ bot.js
@@ -7,9 +7,9 @@ client.on('ready', () => {
 
 client.on('message', message => {
     if (message.content === 'ping') {
-    	message.reply('pong');
+    	message.reply('nge pong');
   	}
 });
 
 // THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);//where BOT_TOKEN is the token of our bot 
+client.login(process.env.BOT_TOKEN);//where BOT_TOKEN is the token of our bot
