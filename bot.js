178f8f452623c9dd6a46454a6f1f8420b22566fd
Didy Septiyono <didy@didy.project>
2018-07-20 07:34:27 +0700

    test

--- bot.js
+++ bot.js
@@ -6,20 +6,37 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-  // If the message is "ping"
-  if (message.content === 'ping') {
-    // Send "pong" to the same channel
-    message.channel.send('saya pong');
-    message.channel.send("username "+message.author.username);
-    message.channel.send("avatar "+message.author.avatar);
-    message.channel.send("client "+message.author.client);
-    message.channel.send("createdAt "+message.author.createdAt);
-    message.channel.send("createdTimestamp "+message.author.createdTimestamp);
-    message.channel.send("email "+message.author.email);
-    message.channel.send("id "+message.author.id);
-    message.channel.send("verified "+message.author.verified);
-    message.channel.send("user "+message.author.user);
-    message.channel.send("users "+message.author.users);
+  // // If the message is "ping"
+  // if (message.content === 'ping') {
+  //   // Send "pong" to the same channel
+  //   message.channel.send('saya pong');
+  //   message.channel.send("username "+message.author.username);
+  //   message.channel.send("avatar "+message.author.avatar);
+  //   message.channel.send("client "+message.author.client);
+  //   message.channel.send("createdAt "+message.author.createdAt);
+  //   message.channel.send("createdTimestamp "+message.author.createdTimestamp);
+  //   message.channel.send("email "+message.author.email);
+  //   message.channel.send("id "+message.author.id);
+  //   message.channel.send("verified "+message.author.verified);
+  //   message.channel.send("user "+message.author.user);
+  //   message.channel.send("users "+message.author.users);
+  // }
+  switch (message.content) {
+    case "ping" :
+      message.channel.send('Pong!');
+      break;
+    case "blah" :
+      message.channel.send("username "+message.author.username);
+      message.channel.send("avatar "+message.author.avatar);
+      message.channel.send("client "+message.author.client);
+      message.channel.send("createdAt "+message.author.createdAt);
+      message.channel.send("createdTimestamp "+message.author.createdTimestamp);
+      message.channel.send("email "+message.author.email);
+      message.channel.send("id "+message.author.id);
+      message.channel.send("verified "+message.author.verified);
+      message.channel.send("user "+message.author.user);
+      message.channel.send("users "+message.author.users);
+      break;
   }
 });
 
