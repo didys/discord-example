4bc3028974851d05544b885dbb13690d03ffb8e6
Didy Septiyono <didy@didy.project>
2018-07-20 09:31:06 +0700

    test

--- bot.js
+++ bot.js
@@ -56,17 +56,12 @@ client.on('message', message => {
       message.channel.send({embed});
       break;
     case "!setaddress" :
-      message.channel.send('saya pong');
-      message.channel.send("username "+message.author.username);
-      message.channel.send("avatar "+message.author.avatar);
-      message.channel.send("client "+message.author.client);
-      message.channel.send("createdAt "+message.author.createdAt);
-      message.channel.send("createdTimestamp "+message.author.createdTimestamp);
-      message.channel.send("email "+message.author.email);
-      message.channel.send("id "+message.author.id);
-      message.channel.send("verified "+message.author.verified);
-      message.channel.send("user "+message.author.user);
-      message.channel.send("users "+message.author.users);
+      // message.channel.send('saya pong');
+      // message.channel.send("username "+message.author.username);
+      // message.channel.send("avatar "+message.author.avatar);
+      // message.channel.send("createdAt "+message.author.createdAt);
+      // message.channel.send("createdTimestamp "+message.author.createdTimestamp);
+      // message.channel.send("id "+message.author.id);
       if (message.author.createdTimestamp <= "1518329862025") { //1530403200000 July 2017
         const embed = new Discord.RichEmbed()
           .setColor('#ffc107')
